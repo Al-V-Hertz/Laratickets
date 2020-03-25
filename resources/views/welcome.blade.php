0a5c400c4d7b78be9b5340e539cd0fa52c12071c
Al-V-Hertz <albert.alay-ay@acacia-soft.com>
2020-03-25 14:07:27 +0800

    Changed the Web name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Tickets
+                    &#119139; Ticket Talk &#119139;
                 </div>
                 
             </div>
